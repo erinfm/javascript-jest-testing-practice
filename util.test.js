a56cbdf672c5511f5281e96395aba2ee3a14695c
Erin M <erinfranmc@gmail.com>
2019-10-08 17:31:34 +0100

    integration testing

--- util.test.js
+++ util.test.js
@@ -1,13 +1,11 @@
-const { generateText } = require("./util");
+const { generateText, checkAndGenerate } = require("./util");
 
 test("should output name and age", () => {
   const text = generateText("Max", 29);
   expect(text).toBe("Max (29 years old)");
-  const text2 = generateText("Anna", 28);
-  expect(text2).toBe("Anna (28 years old)");
 });
 
-test("should output data-less text", () => {
-  const text = generateText("", null);
-  expect(text).toBe(" (null years old)");
+test("should generate a valid text output", () => {
+  const text = checkAndGenerate("Max", 29);
+  expect(text).toBe("Max (29 years old)");
 });
